11e923fa715b8a7af36a755c089d93a3afe7666d
Shivam Bisht <shvmbisht@gmail.com>
2021-02-25 00:51:47 +0530

    integerating jwt authorization

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,29 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  ValidationPipe,
+  Req,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth.credentialsdto';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('/signup')
+  signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+    return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  signIn(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(authCredentialsDto);
+  }
+}
